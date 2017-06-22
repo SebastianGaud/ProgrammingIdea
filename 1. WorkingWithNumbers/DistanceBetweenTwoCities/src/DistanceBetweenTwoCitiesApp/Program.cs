c0ee59c3c2de84ec41686aea631e9d69c7046fbf
WEB\sebastiano.gaudeano <sebastiano.gaudeano@vem.com>
2017-06-22 12:45:29 +0200

    Complete Console Program To Test Library

--- 1. WorkingWithNumbers/DistanceBetweenTwoCities/src/DistanceBetweenTwoCitiesApp/Program.cs
+++ 1. WorkingWithNumbers/DistanceBetweenTwoCities/src/DistanceBetweenTwoCitiesApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DistanceBetweenTwoCitiesLib;
 
 namespace DistanceBetweenTwoCitiesApp
 {
@@ -6,7 +7,35 @@ namespace DistanceBetweenTwoCitiesApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           City city1 = new City(CreateNewCoordinate());
+           City city2 = new City(CreateNewCoordinate());
+
+           Console.WriteLine("Distance between two cities is:" + city1.CalculateDistanceFromAnotherCityInKM(city2)+ " KM");
+        }
+
+        private static Coordinate CreateNewCoordinate()
+        {
+            
+            double latitude, longitude;
+            Console.WriteLine("Insert Latitude:");
+            if(Double.TryParse(Console.ReadLine(), out latitude)){
+                Console.WriteLine("Okay, move on");
+            }
+            else
+            {
+                Console.WriteLine("Coordinate can be only numeric value!");
+            }
+
+            Console.WriteLine("Insert Longitude: ");
+            if(Double.TryParse(Console.ReadLine(), out longitude)){
+                Console.WriteLine("Okay, move on");
+            }
+            else
+            {
+                Console.WriteLine("Coordinate can be only numeric value!");
+            }
+
+            return new Coordinate(latitude, longitude);
         }
     }
 }
