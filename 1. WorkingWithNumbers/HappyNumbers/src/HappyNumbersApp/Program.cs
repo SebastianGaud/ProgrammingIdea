7588bc1b36018d9e4c89bdf2b49424ab6b68c6b8
WEB\sebastiano.gaudeano <sebastiano.gaudeano@vem.com>
2017-06-23 10:55:01 +0200

    Finish to code Program to test Happy numberLib

--- 1. WorkingWithNumbers/HappyNumbers/src/HappyNumbersApp/Program.cs
+++ 1. WorkingWithNumbers/HappyNumbers/src/HappyNumbersApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HappyNumbersLib;
 
 namespace HappyNumbersApp
 {
@@ -6,7 +7,27 @@ namespace HappyNumbersApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfIteration = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                bool? isHappy = HappyNumbersUtility.IsHappyNumber(i, ref i, ref numberOfIteration);
+                string message;
+                if (isHappy == null)
+                {
+                    message = "Number of iteration before valuate this number as non Happy are : " + numberOfIteration +" iterations";
+                }
+                else{
+                    message = isHappy.Value.ToString();
+                }
+
+                //Because i don't really want to see all non-Happy numbers! I make the code above this line only for
+                //diagnostic porpouse.
+                if (isHappy.HasValue? isHappy.Value : false)
+                {
+                    System.Console.WriteLine($"The number :  {i} is this number happy?  {message}");
+                }
+                numberOfIteration = 0;
+            }
         }
     }
 }
