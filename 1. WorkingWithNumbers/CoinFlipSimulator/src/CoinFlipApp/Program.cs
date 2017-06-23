27e2b95823f5221c8b4808d7c02094086f99849f
WEB\sebastiano.gaudeano <sebastiano.gaudeano@vem.com>
2017-06-23 14:51:40 +0200

    Develop program to test CoinFlipUtility

--- 1. WorkingWithNumbers/CoinFlipSimulator/src/CoinFlipApp/Program.cs
+++ 1. WorkingWithNumbers/CoinFlipSimulator/src/CoinFlipApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using CoinFlipLib;
 
 namespace CoinFlipApp
 {
@@ -6,7 +8,44 @@ namespace CoinFlipApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Coin coin = new Coin();
+            CoinFlipUtility flipUtility = new CoinFlipUtility(coin);
+            
+            while (true)
+            {
+                FlipCoin(flipUtility, coin);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        public static void FlipCoin(CoinFlipUtility flipUtility, Coin coin){
+            flipUtility.FlipCoin();
+            System.Console.WriteLine(coin);
+            
+            if (flipUtility.NumberOfHeads > flipUtility.NumberOfTails)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("Number of Heads: "+ flipUtility.NumberOfHeads);
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Number of tails: "+ flipUtility.NumberOfTails);
+                Console.ResetColor();
+            }                
+            else if (flipUtility.NumberOfHeads < flipUtility.NumberOfTails)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Number of Heads: "+ flipUtility.NumberOfHeads);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("Number of tails: "+ flipUtility.NumberOfTails);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("Number of Heads: "+ flipUtility.NumberOfHeads);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                System.Console.WriteLine("Number of tails: "+ flipUtility.NumberOfTails);
+                Console.ResetColor();
+            }
         }
     }
 }
