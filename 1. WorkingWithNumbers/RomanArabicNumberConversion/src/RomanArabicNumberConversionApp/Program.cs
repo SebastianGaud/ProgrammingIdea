7b26404c404bee4be438de6885e1b05cea4de363
Sebastiano <sebastiano.gaudeano@live.com>
2017-06-24 18:58:33 +0200

    Add Project Structure for RomanArabicNumberConversion

---
+++ 1. WorkingWithNumbers/RomanArabicNumberConversion/src/RomanArabicNumberConversionApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanArabicNumberConversionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
