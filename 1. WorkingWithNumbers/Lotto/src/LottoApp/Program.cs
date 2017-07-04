8f1492d1ef4035535e16d9adf870e1796ab4cb53
Sebastiano <sebastiano.gaudeano@live.com>
2017-07-04 11:10:32 +0200

    No Commit

---
+++ 1. WorkingWithNumbers/Lotto/src/LottoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LottoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 1. WorkingWithNumbers/Lotto/src/LottoLib/PrizePool.cs
@@ -0,0 +1,9 @@
+namespace LottoLib
+{
+    public class PrizePool
+    {
+        public decimal PrizePoolValue { get; set; }
+        public decimal GameQuote { get; set; }
+        
+    }
+}
