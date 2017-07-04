3b8df94be5901a37accb527ba4444384c621d2c1
WEB\sebastiano.gaudeano <sebastiano.gaudeano@vem.com>
2017-07-04 14:47:01 +0200

    Add Project Structure for NeonNumber

---
+++ 1. WorkingWithNumbers/NeonNumber/src/NeonNumberApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeonNumberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
