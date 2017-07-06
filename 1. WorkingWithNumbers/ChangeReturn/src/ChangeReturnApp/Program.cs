02f4b8ea62a0ab0b53eda142a2381750fb25e16e
WEB\sebastiano.gaudeano <sebastiano.gaudeano@vem.com>
2017-07-06 16:30:18 +0200

    Test Utility project with console App

--- 1. WorkingWithNumbers/ChangeReturn/src/ChangeReturnApp/Program.cs
+++ 1. WorkingWithNumbers/ChangeReturn/src/ChangeReturnApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ChangeReturnLib;
 
 namespace ChangeReturnApp
 {
@@ -6,7 +7,22 @@ namespace ChangeReturnApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Type price of an item");
+            var price = Convert.ToDecimal(Console.ReadLine());
+
+            System.Console.WriteLine("How much customer give you?");
+            var customerPay = Convert.ToDecimal(Console.ReadLine());
+
+            var listOfAllCoinsChange = ChangeUtility.GetChange(customerPay-price);
+
+            System.Console.WriteLine($"The difference between price and customer money is: {customerPay-price}");
+
+            System.Console.WriteLine("You must give to the customer: ");
+            System.Console.WriteLine(String.Format("{0} Pieces of {1}", listOfAllCoinsChange[CoinsEnum.Penny], CoinsEnum.Penny));
+            System.Console.WriteLine(String.Format("{0} Pieces of {1}", listOfAllCoinsChange[CoinsEnum.Nickel], CoinsEnum.Nickel));
+            System.Console.WriteLine(String.Format("{0} Pieces of {1}", listOfAllCoinsChange[CoinsEnum.Dime], CoinsEnum.Dime));
+            System.Console.WriteLine(String.Format("{0} Pieces of {1}", listOfAllCoinsChange[CoinsEnum.Quarter], CoinsEnum.Quarter));
+            System.Console.WriteLine(String.Format("{0} Pieces of {1}", listOfAllCoinsChange[CoinsEnum.Dollar], CoinsEnum.Dollar));
         }
     }
 }
