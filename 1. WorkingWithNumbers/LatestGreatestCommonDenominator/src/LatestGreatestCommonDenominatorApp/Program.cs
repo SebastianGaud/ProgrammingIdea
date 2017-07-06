4b9310b6896810a3feb637641b44453c5a6abeaa
WEB\sebastiano.gaudeano <sebastiano.gaudeano@vem.com>
2017-07-06 17:13:21 +0200

    Create Standard Folder Structure

---
+++ 1. WorkingWithNumbers/LatestGreatestCommonDenominator/src/LatestGreatestCommonDenominatorApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LatestGreatestCommonDenominatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
