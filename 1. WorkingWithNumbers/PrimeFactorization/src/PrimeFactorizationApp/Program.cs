6c0123bebffee760e9aaff19e96413ced4e5f9d2
WEB\sebastiano.gaudeano <sebastiano.gaudeano@live.com>
2017-07-07 15:28:40 +0200

    Develop Console applicationt to test lib

--- 1. WorkingWithNumbers/PrimeFactorization/src/PrimeFactorizationApp/Program.cs
+++ 1. WorkingWithNumbers/PrimeFactorization/src/PrimeFactorizationApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using PrimeFactorizationLib;
 
 namespace PrimeFactorizationApp
 {
@@ -6,7 +8,17 @@ namespace PrimeFactorizationApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Insert number: ");
+            var choice = Convert.ToInt32(Console.ReadLine());
+            var primeFactors = new Dictionary<int, int>();
+
+            foreach (var item in PrimeFactorization.GetPrimeFactors(choice))
+            {
+                System.Console.WriteLine(String.Format("There is {0} of {1}.", item.Value, item.Key));
+            }
         }
+
+
+        
     }
 }
