bc1c671b9d34007bdc0808936059c22ee7e6e9b8
WEB\sebastiano.gaudeano <sebastiano.gaudeano@live.com>
2017-07-07 11:58:09 +0200

    Add Standard Folder Structure for FindPiToNthDigit

---
+++ 1. WorkingWithNumbers/FindPiToNthDigit/src/FindPiToNthDigitApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindPiToNthDigitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
