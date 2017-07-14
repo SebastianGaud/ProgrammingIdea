3f6d58a33e3bc978643f48fd61aa3cdd6aa493e3
WEB\sebastiano.gaudeano <sebastiano.gaudeano@live.com>
2017-07-14 12:16:05 +0200

    Add Standard project structure for Unit converter

---
+++ 1. WorkingWithNumbers/UnitConverter/src/UnitConverterApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitConverterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
